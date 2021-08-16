2065d808f0bfd432da60184fb7195051a3a9e1f7
Alan Lindsay <alanlindsay@gmail.com>
2021-08-16 14:05:01 -0700

    Initial commit

---
+++ libs/shared/ui-components/src/lib/shared-ui-components.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface SharedUiComponentsProps {}
+
+export function SharedUiComponents(props: SharedUiComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to SharedUiComponents!</h1>
+    </div>
+  );
+}
+
+export default SharedUiComponents;
